00ad30525ec7a2cf8dc89509a86f4462043960c4
Agustin <agustinduranse@gmail.com>
2022-06-17 15:35:59 -0700

    Outlined the high-level tasks.

---
+++ src/main/java/com/tlglearning/playingcards/CardTrick.java
@@ -0,0 +1,17 @@
+package com.tlglearning.playingcards;
+
+import com.tlglearning.playingcards.model.Deck;
+
+public class CardTrick {
+
+    public static void main(String[] args) {
+        //todo create an instance of deck and shuffle it
+        Deck deck = new Deck();
+        deck.shuffle();
+        //todo Draw cards from the deck, according to the instructions, placing ever other card into a red DEQUE and black DEQUE
+        //TODO  (optional: shuffle the red deque and black deque.)
+        //Todo generate a random integer between 0 and the smaller of the two deque sizes and swap that number of cards between the two.
+        //todo count the re cards in the red deque and the black cards in the balck deque, and compare them. they must be equal.
+        //todo sort each deque by color, suit, and rank, and print htem out, along wwith the counts from the previous steps.
+            }
+}
