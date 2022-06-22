cd59e7221db86308d10011b9c35a857d39d29ca3
Agustin <agustinduranse@gmail.com>
2022-06-22 09:34:52 -0700

    put comparator a static nested class in cardtrick.

--- src/main/java/com/tlglearning/playingcards/CardTrick.java
+++ src/main/java/com/tlglearning/playingcards/CardTrick.java
@@ -72,14 +72,14 @@ public class CardTrick {
     }
 
 
-}
-class DisplayComparator implements Comparator<Card> {
+    private static class DisplayComparator implements Comparator<Card> {
 
-    @Override
-    public int compare(Card card1, Card card2) {
-        int comparison = card1.getSuit().getColor().compareTo(card2.getSuit().getColor());
-        comparison = (comparison != 0) ? comparison : card1.getSuit().compareTo(card2.getSuit());
-        comparison = (comparison != 0) ? comparison : card1.getRank().compareTo(card2.getRank());
-        return comparison;
+        @Override
+        public int compare(Card card1, Card card2) {
+            int comparison = card1.getSuit().getColor().compareTo(card2.getSuit().getColor());
+            comparison = (comparison != 0) ? comparison : card1.getSuit().compareTo(card2.getSuit());
+            comparison = (comparison != 0) ? comparison : card1.getRank().compareTo(card2.getRank());
+            return comparison;
+        }
     }
 }
